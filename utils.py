c0abf81bdd059df448f58f5fb4611859f48d29b7
yuncong_li <yuncong_li@kingdee.com>
2018-01-24 10:14:08 +0800

    fix 用pickle替换cPickle

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import codecs
 import os
 import collections
-from six.moves import cPickle
+import pickle
 import numpy as np
 
 
@@ -34,13 +34,13 @@ class TextLoader():
         self.vocab_size = len(self.chars)
         self.vocab = dict(zip(self.chars, range(len(self.chars))))
         with open(vocab_file, 'wb') as f:
-            cPickle.dump(self.chars, f)
+            pickle.dump(self.chars, f)
         self.tensor = np.array(list(map(self.vocab.get, data)))
         np.save(tensor_file, self.tensor)
 
     def load_preprocessed(self, vocab_file, tensor_file):
         with open(vocab_file, 'rb') as f:
-            self.chars = cPickle.load(f)
+            self.chars = pickle.load(f)
         self.vocab_size = len(self.chars)
         self.vocab = dict(zip(self.chars, range(len(self.chars))))
         self.tensor = np.load(tensor_file)
